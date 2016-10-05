9163c2ac010513158356d630b1e8162ab759f854
narendranath1150 <narendranath.mca@gmail.com>
2016-10-05 19:29:32 +0530

    New

--- app.js
+++ app.js
@@ -767,7 +767,7 @@ function sendHelpMessage(event){
 
             "message":{
                 "text":msg,
-                "text":msg,
+                "text":"msg",
                 "quick_replies":[
                   {
                     "content_type":"text",
