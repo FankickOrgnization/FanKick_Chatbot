00bbce6f92ebf8fadfb469e76312fa03cbd06fd5
narendranath1150 <narendranath.mca@gmail.com>
2016-10-06 12:58:44 +0530

    new

--- app.js
+++ app.js
@@ -539,7 +539,7 @@ function sendContentPacks(categoryName, event) {
             "message": {
                 "attachment": {
                     "type": "template",
-                    "text":"We have some cool stuff waiting for you..",
+                    "title":"We have some cool stuff waiting for you..",
                     "payload": {
                         "template_type": "generic",
                         "elements": contentList
