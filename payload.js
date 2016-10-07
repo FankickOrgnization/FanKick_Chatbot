e1b26bdbfb8d32f3730b864d2fd724093f49bb6c
narendranath1150 <narendranath.mca@gmail.com>
2016-10-07 14:27:32 +0530

    new

--- payload.js
+++ payload.js
@@ -424,7 +424,71 @@ function callSendAPI(body, url) {
     });
 }
 
+function sendHelpMessage(event){
+    var userid = event.sender.id;
+    var url = 'https://graph.facebook.com/v2.6/' + userid + '?fields=first_name,last_name,locale,timezone,gender&access_token=' + fbpage_access_token + '';
+    console.log("url", url);
+    request({
+        "uri": url,
+        "method": 'GET'
 
+    }, function(error, response, body) {
+        var userprofiledata = JSON.parse(response.body);
+        var username = userprofiledata.first_name;
+        //console.log("--------:Response data:-------- ", JSON.stringify(body));
+        console.log("--------:Response data:--------first_name ", userprofiledata.first_name);
+        console.log("--------:Response data:--------last_name ", userprofiledata.last_name);
+        console.log("--------:Response data:--------locale ", userprofiledata.locale);
+        console.log("--------:Response data:-------- timezone", userprofiledata.timezone);
+        console.log("--------:Response data:--------gender ", userprofiledata.gender);
+        var senderID = event.sender.id;
+        //var msg = 'Hi '+username+', A lot of exciting things are awaiting for you! Get kicking!';
+        //var msg = 'Hi '+username+'! My name is Kicker.\n How may I come of any help to you today?';
+        var msg = 'Hey '+username+', How are you? \n \nDid you check these amazingly cool stuff on Fankick?';
+
+
+        console.log("--------:Response data:--------gender ", msg);
+        var messageData = {
+            "recipient": {
+                "id": senderID
+            },
+
+            "message":{
+                "text":msg,
+                //"text":"msg",
+                "quick_replies":[
+                  {
+                    "content_type":"text",
+                    "title":"Politics",
+                    "payload":"Politics"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Celebrities",
+                    "payload":"Celebrities"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Movies",
+                    "payload":"Movies"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Sports",
+                    "payload":"Sports"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Fan Magazine",
+                    "payload":"Fan Magazine"
+                  }
+
+                ]
+              }
+            }
+         callSendAPI(messageData,'https://graph.facebook.com/v2.6/592208327626213/messages');
+    });
+}
 
 
 
