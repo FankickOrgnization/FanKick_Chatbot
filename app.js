ab8dad38fa5cf2e968fd3d89386a3fb267c5b700
narendranath1150 <narendranath.mca@gmail.com>
2016-10-13 16:31:27 +0530

    New

--- app.js
+++ app.js
@@ -319,6 +319,11 @@ function questionsPacks(categoryName, event) {
               },
               "message":{
                   "text":rows[1].item_name,
+                  "buttons":[
+                              {
+                                "type":"element_share"
+                              }
+                            ],
                   "quick_replies":[
                     {
                       "content_type":"text",
