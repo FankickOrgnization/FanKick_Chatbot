ff914046765fb303f4e4532909e1072f5f30398f
narendranath1150 <narendranath.mca@gmail.com>
2016-12-21 15:29:59 +0530

    New Commit

--- search.js
+++ search.js
@@ -135,7 +135,7 @@ var url = 'https://kgsearch.googleapis.com/v1/entities:search?query=' + category
             },
 
             "message":{
-                "text":'Hi are you search for ' + categoryName + '',
+                "text":'I assume that you are a fan of ' + categoryName + ' browse the amazing stuff we have on ' + categoryName + '',
                 //"text":"msg",
                 "quick_replies":[
                   {
