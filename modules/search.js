c0a0acabb74f6b28b57f2d6f4fb0f6ff1a5dbae7
narendranath1150 <narendranath.mca@gmail.com>
2016-12-26 14:44:34 +0530

    new commit

---
+++ modules/search.js
@@ -0,0 +1,367 @@
+'use strict';
+var request = require('request');
+//var app = express();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    connectionLimit : 2,
+    host: 'ap-cdbr-azure-southeast-a.cloudapp.net',
+    user: 'bb603e8108da6e',
+    password: '3e384329',
+    database: 'rankworlddev'
+});
+var fbpage_access_token = 'EAAXcJew5yNkBAAvFD3wX3RZACdvA4lZB6XStBzliKI9y4m7I1taAnWUWBezVarL8FjteZCztMBjXZCs35lAweqmc2XZARIf378LZA5lTg5xIebmBmFL4MmJGU4JrowfdkkKDbjqwuzBkCWPxQjgddrW4EZBnv6LiccAHdqoLUNcsgZDZD';
+//
+// var name = "Narendranath";
+//
+// var moviesObj =  [
+//   {
+//     "name": "Quizzes",
+//     "qus": "Name the director of PK?"
+// },
+// {
+//     "name": "Fan Clubs",
+//     "qus": "Join Pretty Alia Club"
+// },
+// {
+//     "name": "Gossip Corner",
+//     "qus": "Anupam Kher slams Om Puri???"
+// },
+// {
+//     "name": "Fan Magazines",
+//     "qus": "Johnny Depp"
+// }
+// ];
+//
+// var quickMenu = [
+//   {
+//     "content_type":"text",
+//     "title":"Categories",
+//     "payload":"Categories"
+//   },
+//   {
+//     "content_type":"text",
+//     "title":"Fan Clubs",
+//     "payload":"Fan Clubs"
+//   },
+//   {
+//     "content_type":"text",
+//     "title":"Fan Magazine",
+//     "payload":"Fan Magazine"
+//   },
+//   {
+//     "content_type":"text",
+//     "title":"Movies",
+//     "payload":"Movies"
+//   },
+//   {
+//     "content_type":"text",
+//     "title":"Sports",
+//     "payload":"Sports"
+//   },
+//   {
+//     "content_type":"text",
+//     "title":"Celebrities",
+//     "payload":"Celebrities"
+//   },
+//   {
+//     "content_type":"text",
+//     "title":"What can you do?",
+//     "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+//   }
+// ];
+// //app.use(bodyParser.json());
+// //console.log("------:thread:-----");
+//***** Get the Inforamation about celebrities using google knoledge graph API *****
+const googleSearchPacks = (categoryName,event) => {
+  console.log("*************---categoryName----*******", categoryName );
+  var userid = event.sender.id;
+    // var params = {
+    //   'query': 'FIFA',
+    //   'limit': 10,
+    //   'indent': true,
+    //   'key' : 'AIzaSyCavmWhCL_wMeLAKrurcVPUdP0ztgubHZc',
+    // };
+var url = 'https://kgsearch.googleapis.com/v1/entities:search?query=' + categoryName + '&key=AIzaSyCavmWhCL_wMeLAKrurcVPUdP0ztgubHZc&limit=5&indent=True'
+  //  var url = 'https://kgsearch.googleapis.com/v1/entities:search' + userid + '?fields=first_name,last_name,locale,timezone,gender&access_token=' + fbpage_access_token + '';
+//https://kgsearch.googleapis.com/v1/entities:search?query=' + userid + '&key=AIzaSyCavmWhCL_wMeLAKrurcVPUdP0ztgubHZc&limit=10&indent=True
+
+    console.log("url", url);
+    request({
+        "uri": url,
+        "method": 'GET'
+    }, function(error, response, body) {
+        var userprofiledata = JSON.parse(response.body);
+      //  var username = userprofiledata.first_name;
+        //console.log("--------:Response data:-------- ", JSON.stringify(body));
+        console.log("--------:Response data:--------first_name ", userprofiledata.itemListElement);
+        var rows = userprofiledata.itemListElement;
+        var rowlen = rows.length;
+        console.log("--------:Response data:--------length ", rowlen);
+        var senderID = event.sender.id;
+        for (var i = 0; i < 5; i++) { //Construct request body
+          console.log("--------:google Response data:-------- name ", rows[i].result.name);
+          var name1 = rows[0].result.name;
+          var name2 = rows[1].result.name;
+          var name3 = rows[2].result.name;
+          var name4 = rows[3].result.name;
+          var name5 = rows[4].result.name;
+
+          // for (var i = 0; i < moviesObj.length; i++) { //Construct request body
+          //     var keyMap = {
+          //                   "title": moviesObj[i].name,
+          //                   "image_url": moviesObj[i].imgurl,
+          //                             //"item_url": moviesObj[i].imgurl,
+          //                   "buttons": [{
+          //                               "type": "postback",
+          //                               "title": moviesObj[i].name,
+          //                               "payload": moviesObj[i].name
+          //                             }]
+          //                         };
+          //
+          //     contentList.push(keyMap);
+          // }
+
+
+
+          // var keyMpa ={
+          //       "content_type":"text",
+          //       "title":"Categories",
+          //       "payload":"Categories"
+          //     };
+            // var keyMap = {
+            //     "title": rows[i].name,
+            //     console.log("--------:Response data:--------first_name ", rows[i].name);
+            //     // "image_url": rows[i].image_url,
+            //     // "item_url": rows[i].image_url
+            //   //   "buttons": [{
+            //   //       "type": "postback",
+            //   //       "title": "View",
+            //   //       "payload": rows[i].id
+            //   //   }
+            //   //   // , {
+            //   //   //     "type": "postback",
+            //   //   //     "title": "Magazine",
+            //   //   //     "payload": "USER_DEFINED_PAYLOAD"
+            //   //   // }
+            //   // ]
+            // };
+            // contentList.push(keyMap);
+        }
+        //var msg = 'Hey '+username+', How are you? \n \nDid you check these amazingly cool stuff on Fankick?';
+        //console.log("--------:Response data:--------gender ", msg);
+        var messageData = {
+            "recipient": {
+                "id": senderID
+            },
+
+            "message":{
+                "text":'I assume that you are a fan of ' + categoryName + ' browse the amazing stuff we have on ' + categoryName + '',
+                //"text":"msg",
+                "quick_replies":[
+                  {
+                    "content_type":"text",
+                    "title":name1.slice(0,20),
+                    "payload":rows[0].result.name
+                  },
+                  {
+                    "content_type":"text",
+                    "title":name2.slice(0,20),
+                    "payload":rows[1].result.name
+                  },
+                  {
+                    "content_type":"text",
+                    "title":name3.slice(0,20),
+                    "payload":rows[2].result.name
+                  },
+                  {
+                    "content_type":"text",
+                    "title":name4.slice(0,20),
+                    "payload":rows[3].result.name
+                  },
+                  {
+                    "content_type":"text",
+                    "title":name5.slice(0,20),
+                    "payload":rows[4].result.name
+                  }
+                  // ,
+                  // {
+                  //   "content_type":"text",
+                  //   "title":name4.slice(0,20),
+                  //   "payload":rows[5].result.name
+                  // },
+                  // {
+                  //   "content_type":"text",
+                  //   "title":name4.slice(0,20),
+                  //   "payload":rows[6].result.name
+                  // },
+                  // {
+                  //   "content_type":"text",
+                  //   "title":name4.slice(0,20),
+                  //   "payload":rows[7].result.name
+                  // },
+                  // {
+                  //   "content_type":"text",
+                  //   "title":name4.slice(0,20),
+                  //   "payload":rows[8].result.name
+                  // },
+                  // {
+                  //   "content_type":"text",
+                  //   "title":name4.slice(0,20),
+                  //   "payload":rows[9].result.name
+                  // }
+
+                ]
+              }
+            }
+         callSendAPI(messageData,'https://graph.facebook.com/v2.6/592208327626213/messages');
+    });
+}
+
+
+
+function callSendAPI(body, url) {
+    console.log("url", url);
+    console.log("Body", body);
+    request({
+        uri: url,
+        qs: {
+            access_token: fbpage_access_token
+        },
+        method: 'POST',
+        json: body,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }, function(error, response, body) {
+        console.log("Response data: ", JSON.stringify(body));
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+            console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            //console.error(response);
+            console.error("Error while sending message:", error);
+        }
+    });
+}
+
+function sendHelpMessage(event){
+    var userid = event.sender.id;
+    var url = 'https://graph.facebook.com/v2.6/' + userid + '?fields=first_name,last_name,locale,timezone,gender&access_token=' + fbpage_access_token + '';
+    console.log("url", url);
+    request({
+        "uri": url,
+        "method": 'GET'
+
+    }, function(error, response, body) {
+        var userprofiledata = JSON.parse(response.body);
+        var username = userprofiledata.first_name;
+        //console.log("--------:Response data:-------- ", JSON.stringify(body));
+        console.log("--------:Response data:--------first_name ", userprofiledata.first_name);
+        console.log("--------:Response data:--------last_name ", userprofiledata.last_name);
+        console.log("--------:Response data:--------locale ", userprofiledata.locale);
+        console.log("--------:Response data:-------- timezone", userprofiledata.timezone);
+        console.log("--------:Response data:--------gender ", userprofiledata.gender);
+        var senderID = event.sender.id;
+        //var msg = 'Hey '+username+', How are you? \n \nDid you check these amazingly cool stuff on Fankick?';
+      //  console.log("--------:Response data:--------gender ", msg);
+        var messageData = {
+            "recipient": {
+                "id": senderID
+            },
+
+            "message":{
+                "text":"HI",
+                //"text":"msg",
+                "quick_replies":[
+                  // {
+                  //   "content_type":"text",
+                  //   "title":"Politics",
+                  //   "payload":"Politics"
+                  // },
+                  {
+                    "content_type":"text",
+                    "title":"Celebrities",
+                    "payload":"Celebrities"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Movies",
+                    "payload":"Movies"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Sports",
+                    "payload":"Sports"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Fan Clubs",
+                    "payload":"Fan Clubs"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Fan Magazine",
+                    "payload":"Fan Magazine"
+                  }
+
+                ]
+              }
+            }
+         callSendAPI(messageData,'https://graph.facebook.com/v2.6/592208327626213/messages');
+    });
+}
+const sendHelpMessageSecond = (event, userid) => {
+//function sendHelpMessageSecond(event, userid) {
+    var senderID = event.sender.id;
+        var msg = 'Did you check these amazingly cool stuff on Fankick?';
+        var messageData = {
+            "recipient": {
+                "id": senderID
+            },
+            "message":{
+                "text":msg,
+                "quick_replies":[
+                  {
+                    "content_type":"text",
+                    "title":"Categories",
+                    "payload":"Categories"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Movies",
+                    "payload":"Movies"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Sports",
+                    "payload":"Sports"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Music",
+                    "payload":"Music"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Celebrities",
+                    "payload":"Celebrities"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"What can you do?",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                  }
+                ]
+              }
+            }
+         callSendAPI(messageData,'https://graph.facebook.com/v2.6/592208327626213/messages');
+}
+
+module.exports = {
+  googleSearchPacks: googleSearchPacks,
+  //sendHelpMessageSecond: sendHelpMessageSecond
+  // name:name,
+  //quickMenu:quickMenu
+};
---
+++ modules/wit.js
@@ -0,0 +1,212 @@
+'use strict';
+var request = require('request');
+const fb = require('./app.js');
+//const Wit2;
+//fb.myvar = 'Hello Face book**********Hello Face book';
+//fb.test1();
+var name = "foobar";
+var witvalue;
+// export it
+//exports.name = name;
+
+console.log("Hi Welcome to Wit");
+//function wittest(msgwit_value){
+const wittest = (msgwit_value) => {
+  console.log("Hi Welcome to Wit",msgwit_value);
+};
+
+// function displayName() {
+//   function myName() {
+//     return "Sravan Kumar"
+//   }
+//   return myName();
+// }
+var msgwit_value;
+const getwitmessageText = (msgwit) => {
+  request({
+      uri: 'https://api.wit.ai/message?v=20160526&q='+ msgwit,
+      headers: {
+          "Authorization": "Bearer USTWU2HGSIYGK3JBQX6EM2UGEQOS26ZX"
+      }
+  }, function(error, response) {
+      if (error) {
+          console.log("Error While geting response from Wit:", error);
+      } else {
+        var res = JSON.stringify(response);
+        var res_data = response.body;
+        var wit_res_data = JSON.parse(res_data);
+        var wit_res_data_ent = wit_res_data.entities;
+        var wit_res_data_intent =  wit_res_data_ent.intent;
+        var wit_res_data_location = wit_res_data_ent.location;
+        var wit_res_msg_id = wit_res_data.msg_id;
+
+          // console.log("Response from Wit--Reswit", res);
+          // console.log("Response from Wit--msg_idwit", wit_res_data.msg_id);
+          // console.log("Response from Wit************wit1", wit_res_data.entities);
+          // console.log("Response from Wit************wit2", wit_res_data_ent.intent);
+          // console.log("Response from Wit************wit3", wit_res_data_ent.location);
+          // console.log("Response from Wit************wit4", wit_res_data_intent);
+          // console.log("Response from Wit************wit5", wit_res_data_location);
+          //console.log("Response from Wit************6", wit_res_data_intent.value);
+
+          for(var i=0;i<wit_res_data_intent.length;i++)
+          {
+            var td1=wit_res_data_intent[i]["confidence"];
+            var td2=wit_res_data_intent[i]["type"];
+            var td3=wit_res_data_intent[i]["value"];
+          }
+          // console.log("confidence************wit5",td1);
+          // console.log("type************wit5",td2);
+          // console.log("value************wit5", td3);
+        msgwit_value = td3;
+        // console.log('******msgwit_valuewit', msgwit_value);
+        getwitconverse(wit_res_msg_id,msgwit_value,msgwit);
+        //return msgwit_value;
+        witvalue = msgwit_value;
+        exports.witvalue = witvalue;
+        //console.log('******msgwit_valuewit2', witvalue);
+      }
+    //  witvalue = msgwit_value;
+  });
+//  witvalue = msgwit_value;
+};
+
+const getwitmsg = (msg_id,msg_value,msg_text)=> {
+// console.log("msg_id***********Wit", msg_id);
+// console.log("msg_value***********Wit", msg_value);
+// console.log("msg_value***********Wit", msg_text);
+var myJSONObject = {
+  "intent": msg_value
+};
+var hederReq = {
+        "Authorization": "Bearer USTWU2HGSIYGK3JBQX6EM2UGEQOS26ZX",
+        "Content-Type": "application/json",
+         "Accept": "application/json"
+    };
+//uri: 'https://api.wit.ai/converse?v=20160526&session_id='+msg_id+'&q='+msg_text,
+request({
+  url: 'https://api.wit.ai/converse?v=20160526&session_id='+msg_id,
+  method: "POST",
+  json: true,   // <--Very important!!!
+  headers:hederReq,
+  body: myJSONObject
+  },function(error, response, body) {
+    console.log("Wit Ai error",error);
+    console.log("Wit Ai body*************",body);
+    request({
+      url: 'https://api.wit.ai/converse?v=20160526&session_id='+msg_id,
+      method: "POST",
+      json: true,   // <--Very important!!!
+      headers:hederReq,
+      body: myJSONObject
+      },function(error, response, body) {
+        // console.log("Wit Ai error",error);
+        // console.log("Wit Ai body*************jkhjkhjkhjkhjkh",body);
+        //console.log("Wit Ai body1",body.msg);
+        if (!error && response.statusCode == 200) {
+         console.log("Successfully received Wit Message");
+        } else {
+            console.error("Unable to send message.");
+            //console.error(response);
+            console.error("Error while sending message:", error);
+        }
+    });
+    //console.log("Wit Ai body1",body.msg);
+    if (!error && response.statusCode == 200) {
+     console.log("Successfully received Wit Message");
+    } else {
+        console.error("Unable to send message.");
+        //console.error(response);
+        console.error("Error while sending message:", error);
+    }
+});
+}
+// End Geting the Wit cenversion Message ************************
+function getwitconverse(wit_res_msg_id,msgwit_value,msgwit){
+// console.log("getwitconverse***********wit_res_msg_id", wit_res_msg_id);
+// console.log("getwitconverse***********msgwit_value", msgwit_value);
+// console.log("getwitconverse***********msgwit", msgwit);
+var myJSONObject = {
+  "intent": msgwit_value
+};
+var hederReq = {
+        "Authorization": "Bearer USTWU2HGSIYGK3JBQX6EM2UGEQOS26ZX",
+        "Content-Type": "application/json",
+         "Accept": "application/json"
+    };
+//uri: 'https://api.wit.ai/converse?v=20160526&session_id='+msg_id+'&q='+msg_text,
+request({
+  url: 'https://api.wit.ai/converse?v=20160526&session_id='+wit_res_msg_id,
+  method: "POST",
+  json: true,   // <--Very important!!!
+  headers:hederReq,
+  body: myJSONObject
+  },function(error, response, body) {
+    console.log("Wit Ai error",error);
+    console.log("Wit Ai body*************",body);
+    request({
+      url: 'https://api.wit.ai/converse?v=20160526&session_id='+wit_res_msg_id,
+      method: "POST",
+      json: true,   // <--Very important!!!
+      headers:hederReq,
+      body: myJSONObject
+      },function(error, response, body) {
+        // console.log("Wit Ai error",error);
+        // console.log("Wit Ai body*************jkhjkhjkhjkhjkh",body);
+        //console.log("Wit Ai body1",body.msg);
+        if (!error && response.statusCode == 200) {
+        // console.log("Successfully received Wit Message");
+        } else {
+            //console.error("Unable to send message.");
+            //console.error(response);
+            //console.error("Error while sending message:", error);
+        }
+    });
+    //console.log("Wit Ai body1",body.msg);
+    if (!error && response.statusCode == 200) {
+     console.log("Successfully received Wit Message");
+    } else {
+        // console.error("Unable to send message.");
+        // //console.error(response);
+        // console.error("Error while sending message:", error);
+    }
+});
+}
+
+// module.exports = function(initValue) {
+//   return {
+//     addOne: function() {
+//       return initValue + 1;
+//     },
+//     subtractOne: function() {
+//       return initValue - 1;
+//     },
+//   }
+// }
+
+// var value_123 = witvalue;
+// console.log("Unable to send message value_123.",value_123 );
+module.exports = {
+//     test: function() {
+//         console.log('var is', this.myvar);
+//     },
+// function(initValue) {
+//   console.log("initValue",initValue);
+//   // return {
+//   //   addOne: function() {
+//   //     return initValue + 1;
+//   //   },
+//   //   subtractOne: function() {
+//   //     return initValue - 1;
+//   //   },
+//   // }
+//   return "initValue";
+// },
+  // wittest: wittest,
+  // name : name,
+  // value_123 : value_123,
+  // witvalue : witvalue,
+  // getWit: getWit,
+  // getwitmsg: getwitmsg,
+  //getwitmessageText: getwitmessageText
+};
