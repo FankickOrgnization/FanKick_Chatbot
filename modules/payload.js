d89d5e649e8d143abfb56cee81f486000ece1eed
narendranath1150 <narendranath.mca@gmail.com>
2016-12-27 14:09:49 +0530

    New Commit

--- modules/payload.js
+++ modules/payload.js
@@ -701,7 +701,7 @@ function sendHelpMessageSecond(event, userid) {
 
 // const fbuserdetails = (event,userid) =>{
 function fbuserdetails(event, userid) {
-    var url = 'https://graph.facebook.com/v2.6/' + userid + '?fields=first_name,last_name,locale,timezone,gender&access_token=' + fbpage_access_token + '';
+    var url = 'https://graph.facebook.com/v2.6/' + userid + '?fields=first_name,last_name,locale,timezone,gender,location&access_token=' + fbpage_access_token + '';
     console.log("url", url);
     request({
         "uri": url,
