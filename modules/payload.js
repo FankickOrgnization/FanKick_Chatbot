7f7e81b2c0e648b910bb66af31389ca7d8930d46
narendranath1150 <narendranath.mca@gmail.com>
2016-12-28 12:25:03 +0530

    new commit

--- modules/payload.js
+++ modules/payload.js
@@ -236,7 +236,9 @@ const sendContentPacks = (categoryName,event) => {
               for(var i in googleTrends_result){
                 console.log(i.value); // alerts key
                 console.log(googleTrends_result[i]); //alerts key's value
+                var googleTrends_result_obj = googleTrends_result[i]
               }
+              console.log(googleTrends_result_obj[0]);
             //console.log(Object.keys(googleTrends_result));
             // for ( property in googleTrends_result ) {
             //   console.log( property ); // Outputs: foo, fiz or fiz, foo
