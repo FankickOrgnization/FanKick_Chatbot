b975b0165b8e773c356f9fdc1c62af781779d76c
narendranath1150 <narendranath.mca@gmail.com>
2017-02-08 18:57:58 +0530

    new Commit

--- modules/payload.js
+++ modules/payload.js
@@ -588,40 +588,40 @@ function allcategory(event, categoryName){
    try {
       categoryName = "../contentjson/" + categoryName;
       var json  = require(categoryName);
-      var json = {
-            "attachment": {
-              "type": "template",
-                "payload": {
-                  "template_type": "generic",
-                  "elements":[{
-                      "title":"Music",
-                      "image_url":imgdangol,
-                      "subtitle":"Widen your ears! I'm going to make a huge noise, define the tune… :-)",
-                      "buttons":[{
-                        "type": "postback",
-                        "title": "Music",
-                        "payload": "Music"
-                      }]
-                    }]
-                }
-              },
-            "quick_replies":[
-              {
-                "content_type":"text",
-                "title":"Classical Music",
-                "payload":"Music"
-              },
-              {
-                "content_type":"text",
-                "title":"Western Music",
-                "payload":"Music"
-              },
-              {
-                "content_type":"text",
-                "title":"Categories",
-                "payload":"Categories"
-              }]
-      };
+      // var json = {
+      //       "attachment": {
+      //         "type": "template",
+      //           "payload": {
+      //             "template_type": "generic",
+      //             "elements":[{
+      //                 "title":"Music",
+      //                 "image_url":imgdangol,
+      //                 "subtitle":"Widen your ears! I'm going to make a huge noise, define the tune… :-)",
+      //                 "buttons":[{
+      //                   "type": "postback",
+      //                   "title": "Music",
+      //                   "payload": "Music"
+      //                 }]
+      //               }]
+      //           }
+      //         },
+      //       "quick_replies":[
+      //         {
+      //           "content_type":"text",
+      //           "title":"Classical Music",
+      //           "payload":"Music"
+      //         },
+      //         {
+      //           "content_type":"text",
+      //           "title":"Western Music",
+      //           "payload":"Music"
+      //         },
+      //         {
+      //           "content_type":"text",
+      //           "title":"Categories",
+      //           "payload":"Categories"
+      //         }]
+      // };
 
       var fullMessage = { recipient: { id: senderID }};
       fullMessage.message = json;
