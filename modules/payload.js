8aaa0bd878219c00d7f89625be78cfb04e970047
narendranath1150 <narendranath.mca@gmail.com>
2017-02-09 15:39:29 +0530

    new commit

--- modules/payload.js
+++ modules/payload.js
@@ -867,31 +867,31 @@ function fbuserdetails(event, userid) {
         //   });
         //   });
 
-          pool.getConnection(function(err, connection) {
-            connection.query('INSERT INTO cc_user_preference(facebookId, firstName, lastName, fullName, gender, locale, timeZone)VALUES(?,?,?,?,?,?,?)',[senderID, userfname, userlname, userFullName, userfbdata.gender, userfbdata.locale,userfbdata.timezone], function(err, rows) {
-                if (err) {
-                    console.log("Error While retriving content pack data from database:", err);
-                } else {
-                    console.log("No Data Found From Database");
-                    //sendHelpMessage(event);
-                    //sendImageMessage(event);
-                }
-                connection.release();
-            });
-            });
-
-            pool.getConnection(function(err, connection) {
-              connection.query('update cc_user_preference set language="Telugu" where facebookId=?',[senderID], function(err, rows) {
-                  if (err) {
-                      console.log("Error While retriving content pack data from database:", err);
-                  } else {
-                      console.log("No Data Found From Database");
-                      sendHelpMessage(event);
-                      //sendImageMessage(event);
-                  }
-                  connection.release();
-              });
-              });
+          // pool.getConnection(function(err, connection) {
+          //   connection.query('INSERT INTO cc_user_preference(facebookId, firstName, lastName, fullName, gender, locale, timeZone)VALUES(?,?,?,?,?,?,?)',[senderID, userfname, userlname, userFullName, userfbdata.gender, userfbdata.locale,userfbdata.timezone], function(err, rows) {
+          //       if (err) {
+          //           console.log("Error While retriving content pack data from database:", err);
+          //       } else {
+          //           console.log("No Data Found From Database");
+          //           //sendHelpMessage(event);
+          //           //sendImageMessage(event);
+          //       }
+          //       connection.release();
+          //   });
+          //   });
+          //
+          //   pool.getConnection(function(err, connection) {
+          //     connection.query('update cc_user_preference set language="Telugu" where facebookId=?',[senderID], function(err, rows) {
+          //         if (err) {
+          //             console.log("Error While retriving content pack data from database:", err);
+          //         } else {
+          //             console.log("No Data Found From Database");
+          //             //sendHelpMessage(event);
+          //             //sendImageMessage(event);
+          //         }
+          //         connection.release();
+          //     });
+          //     });
 
         //fbuserlocation();
         //var msg = 'Hi '+username+', A lot of exciting things are awaiting for you! Get kicking!';
