5cc118602c8405d7ce7949b62ce03c10454bfbb0
narendranath1150 <narendranath.mca@gmail.com>
2017-02-09 11:04:06 +0530

    new commit

--- modules/payload.js
+++ modules/payload.js
@@ -586,40 +586,40 @@ function allcategory(event, categoryName){
   var senderID = event.sender.id;
   var imgdangol = 'http://t3.gstatic.com/images?q=tbn:ANd9GcQIXnFlBKGWT1ByyIu3qfxX6opQX6BmeeU_qsiE3X8rX9ZRr63r';
    try {
-      categoryName = "../contentjson/" + categoryName;
-      var json  = require(categoryName);
-      // var json = {
-      //       "text": "Movie is the king of entertainment. Pick your favorite kingdom (You can select multiple)… :-).",
-      //       "quick_replies": [
-      //         {
-      //           "content_type":"text",
-      //           "title":"Hollywood",
-      //           "payload":"Hollywood"
-      //         },
-      //         {
-      //           "content_type":"text",
-      //           "title":"Bollywood",
-      //           "payload":"Bollywood"
-      //         },
-      //         {
-      //           "content_type":"text",
-      //           "title":"Tollywood",
-      //           "payload":"Tollywood"
-      //         },
-      //         {
-      //           "content_type":"text",
-      //           "title":"Kollywood",
-      //           "payload":"Kollywood"
-      //         },
-      //         {
-      //           "content_type":"text",
-      //           "title":"Categories",
-      //           "payload":"Categories"
-      //         },
-      //         {
-      //           "content_type":"location",
-      //         }]
-      // };
+      //categoryName = "../contentjson/" + categoryName;
+      //var json  = require(categoryName);
+      var json = {
+            "text": "Movie is the king of entertainment. Pick your favorite kingdom (You can select multiple)… :-).",
+            "quick_replies": [
+              {
+                "content_type":"text",
+                "title":"Hollywood",
+                "payload":"Hollywood"
+              },
+              {
+                "content_type":"text",
+                "title":"Bollywood",
+                "payload":"Bollywood"
+              },
+              {
+                "content_type":"text",
+                "title":"Tollywood",
+                "payload":"Tollywood"
+              },
+              {
+                "content_type":"text",
+                "title":"Kollywood",
+                "payload":"Kollywood"
+              },
+              {
+                "content_type":"text",
+                "title":"Categories",
+                "payload":"Categories"
+              },
+              {
+                "content_type":"location",
+              }]
+      };
       var fullMessage = { recipient: { id: senderID }};
       fullMessage.message = json;
       callSendAPI(fullMessage,'https://graph.facebook.com/v2.6/592208327626213/messages');
