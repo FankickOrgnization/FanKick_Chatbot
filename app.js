2591577e0c37b187b0aa7ab23a8686516ab427cc
narendranath1150 <narendranath.mca@gmail.com>
2017-03-06 20:00:54 +0530

    new commit

--- app.js
+++ app.js
@@ -293,8 +293,8 @@ function celebritypics(messagingEvent,quickpayloadtext){
   console.log("actername",actername);
   console.log("type",subCategory);
   pool.getConnection(function(err, connection) {
-  connection.query('select * from cc_film_celebrity_preference where name = ?',[aname], function(err, rows) {
-    console.log("********filmactor*********", aname);
+  connection.query('select * from cc_film_celebrity_preference where name = ?',[actername], function(err, rows) {
+    console.log("********filmactor*********", actername);
       //console.log("*************************-after", categoryName);
       console.log("*************************filmactor", rows);
       if (err) {
