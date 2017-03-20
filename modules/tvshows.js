6c3fadbfe0a5ee1439eb734a559a51d9b82d7cb7
narendranath1150 <narendranath.mca@gmail.com>
2017-03-20 16:33:56 +0530

    new commit

--- modules/tvshows.js
+++ modules/tvshows.js
@@ -1,18 +1,11 @@
 'use strict';
 var request = require('request');
 const searchText = require('./search.js');
-//const thread = require('./thread.js');
 var googleTrends = require('google-trends-api');
 const errors = require('../contentjson/errormsg.json');
-//const movie = require('../contentjson/movies.json');
-//const sport = require('../contentjson/sports.json');
-//const tv_show = require('../contentjson/tv shows.json');
-//const musics = require('../contentjson/music.json');
 const jokes = require('../contentjson/jokes.json');
 const fbRquest = require('./fbapi.js');
-//const mysqlconfig = require('./mysqlconfig.js');
 const dbpool = require('./mysqlconfig.js');
-//var app = express();
 var mysql = require('mysql');
 //var pool = mysql.createPool({connectionLimit: 1, host: 'ap-cdbr-azure-southeast-a.cloudapp.net', user: 'bb603e8108da6e', password: '3e384329', database: 'rankworlddev'});
 var pool = dbpool.mysqlpool;
@@ -40,8 +33,6 @@ var quickreply = [
 //function tvshowsinfo(messagingEvent, moviename)
 const tvshowinfo = (messagingEvent, tvshowname) => {
     var senderID = messagingEvent.sender.id;
-    //var img = 'https://fankickdev.blob.core.windows.net/images/home_logo.png';
-    //var msg = 'Amazing talent! Here is what I know about '+img+'';
     var messageData = {
         "recipient": {
             "id": senderID
@@ -50,14 +41,14 @@ const tvshowinfo = (messagingEvent, tvshowname) => {
             "text": "Here you go👉..."
             //"text":msg
         }
-        // "message": {
-        //     "attachment": {
-        //         "type": "audio",
-        //         "payload": {
-        //             "url": "https://petersapparel.com/bin/clip.mp3"
-        //         }
-        //     }
-        // }
+        "message": {
+            "attachment": {
+                "type": "audio",
+                "payload": {
+                    "url": "https://petersapparel.com/bin/clip.mp3"
+                }
+            }
+        }
     };
     fbRquest.callFBAPI(messageData, 'https://graph.facebook.com/v2.6/592208327626213/messages');
     tvshowsdetails(messagingEvent, tvshowname);
