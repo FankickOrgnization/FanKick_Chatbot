2e8c0f0d654ed30e3b94ff964f462fec95c0b410
narendranath1150 <narendranath.mca@gmail.com>
2017-04-21 12:34:51 +0530

    new commit

--- modules/sports.js
+++ modules/sports.js
@@ -194,9 +194,19 @@ function sportsmenu(messagingEvent) {
                 }
                 for (var i = 0; i < rowslenth; i++) { //Construct request body
                     var keyMap = {
-                        "title": rows[i].title,
-                        "image_url": rows[i].imageUrl,
-                        "subtitle": rows[i].celebrity,
+                        // "title": rows[i].title,
+                        // "image_url": rows[i].imageUrl,
+                        // "subtitle": rows[i].celebrity,
+                        "title":"Welcome to Peter\'s Hats",
+                        "image_url":"https://petersfancybrownhats.com/company_image.png",
+                        "subtitle":"We\'ve got the right hat for everyone.",
+                        "default_action": {
+                          "type": "web_url",
+                          "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                          "messenger_extensions": true,
+                          "webview_height_ratio": "tall",
+                          "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                        }
                         "buttons": [
                             {
                                 "type": "postback",
