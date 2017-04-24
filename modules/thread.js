b401b90cbfcd41bce82738588eda5ba2760a1171
narendranath1150 <narendranath.mca@gmail.com>
2017-04-24 14:15:31 +0530

    new commit

--- modules/thread.js
+++ modules/thread.js
@@ -61,7 +61,46 @@ var body = {
       ]
     },
     {
-      "locale":"zh_CN",
+      "locale":"en_US",
+      "composer_input_disabled":false
+    }
+  ],
+  [
+    {
+      "locale":"default",
+      "composer_input_disabled":true,
+      "call_to_actions":[
+        {
+          "title":"My Account",
+          "type":"nested",
+          "call_to_actions":[
+            {
+              "title":"Pay Bill",
+              "type":"postback",
+              "payload":"PAYBILL_PAYLOAD"
+            },
+            {
+              "title":"History",
+              "type":"postback",
+              "payload":"HISTORY_PAYLOAD"
+            },
+            {
+              "title":"Contact Info",
+              "type":"postback",
+              "payload":"CONTACT_INFO_PAYLOAD"
+            }
+          ]
+        },
+        {
+          "type":"web_url",
+          "title":"Latest News",
+          "url":"http://petershats.parseapp.com/hat-news",
+          "webview_height_ratio":"full"
+        }
+      ]
+    },
+    {
+      "locale":"en_GB",
       "composer_input_disabled":false
     }
   ]
