c5b9eeb8167b83df79ebbc391d7597faf88b3459
narendranath1150 <narendranath.mca@gmail.com>
2017-04-24 14:18:11 +0530

    new commit

--- modules/thread.js
+++ modules/thread.js
@@ -65,45 +65,7 @@ var body = {
       "composer_input_disabled":false
     }
   ],
-  [
-    {
-      "locale":"default",
-      "composer_input_disabled":true,
-      "call_to_actions":[
-        {
-          "title":"My Account",
-          "type":"nested",
-          "call_to_actions":[
-            {
-              "title":"Pay Bill",
-              "type":"postback",
-              "payload":"PAYBILL_PAYLOAD"
-            },
-            {
-              "title":"History",
-              "type":"postback",
-              "payload":"HISTORY_PAYLOAD"
-            },
-            {
-              "title":"Contact Info",
-              "type":"postback",
-              "payload":"CONTACT_INFO_PAYLOAD"
-            }
-          ]
-        },
-        {
-          "type":"web_url",
-          "title":"Latest News",
-          "url":"http://petershats.parseapp.com/hat-news",
-          "webview_height_ratio":"full"
-        }
-      ]
-    },
-    {
-      "locale":"en_GB",
-      "composer_input_disabled":false
-    }
-  ]
+  
 };
   request({
       uri: 'https://graph.facebook.com/v2.6/me/thread_settings?access_token='+ accessToken,
