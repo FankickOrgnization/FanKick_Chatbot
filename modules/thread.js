a91bcd4e77f52260530950d1796e68bee3038eaa
narendranath1150 <narendranath.mca@gmail.com>
2017-04-26 18:41:10 +0530

    new commit

--- modules/thread.js
+++ modules/thread.js
@@ -4,93 +4,71 @@ var request = require('request');
 
 const persistentMenu = (accessToken) => {
     console.log("accessToken for thread:-----", accessToken);
-    // var body = {
-    //     "setting_type": "call_to_actions",
-    //     "thread_state": "existing_thread",
-    //     "call_to_actions": [
-    //         {
-    //             "type": "postback",
-    //             "title": "Help",
-    //             "payload": "home"
-    //         }, {
-    //             "title": "Choose Categories",
-    //             "type":"nested",
-    //              "call_to_actions":[
-    //                {
-    //                  "title":"Pay Bill",
-    //                  "type":"postback",
-    //                  "payload":"PAYBILL_PAYLOAD"
-    //                },
-    //                {
-    //                  "title":"History",
-    //                  "type":"postback",
-    //                  "payload":"HISTORY_PAYLOAD"
-    //                },
-    //                {
-    //                  "title":"Contact Info",
-    //                  "type":"postback",
-    //                  "payload":"CONTACT_INFO_PAYLOAD"
-    //                }
-    //              ]
-    //         }, {
-    //             "type": "web_url",
-    //             "title": "Download Fankick App",
-    //             "url": "http://google.com/"
-    //         },{
-    //             "type": "web_url",
-    //             "title": "Fankick website",
-    //             "url": "http://google.com/"
-    //         },{
-    //             "type": "web_url",
-    //             "title": "Fankick App",
-    //             "url": "http://google.com/"
-    //         }
-    //     ]
-    // };
     var body = {
-      "persistent_menu":[
-        {
-          "locale":"default",
-          "composer_input_disabled":true,
-          "call_to_actions":[
-            {
-              "title":"My Account",
-              "type":"nested",
+              "setting_type" : "call_to_actions",
+              "thread_state" : "existing_thread",
               "call_to_actions":[
                 {
-                  "title":"Pay Bill",
                   "type":"postback",
-                  "payload":"PAYBILL_PAYLOAD"
+                  "title":"Help",
+                  "payload":"home"
                 },
                 {
-                  "title":"History",
                   "type":"postback",
-                  "payload":"HISTORY_PAYLOAD"
+                  "title":"Choose Categories",
+                  "payload":"home"
                 },
                 {
-                  "title":"Contact Info",
-                  "type":"postback",
-                  "payload":"CONTACT_INFO_PAYLOAD"
+                  "type":"web_url",
+                  "title":"Download Fankick App",
+                  "url":"http://rankworldlive.azurewebsites.net/shareToSocial?ranklistid=3111"
                 }
               ]
-            },
-            {
-              "type":"web_url",
-              "title":"Latest News",
-              "url":"http://rankworldlive.azurewebsites.net/shareToSocial?ranklistid=3111",
-              "webview_height_ratio":"full"
-            }
-          ]
-        },
-        {
-          "locale":"en_US",
-          "composer_input_disabled":false
-        }
-      ],
-
-    };
+            };
+    // var body = {
+    //   "persistent_menu":[
+    //     {
+    //       "locale":"default",
+    //       "composer_input_disabled":true,
+    //       "call_to_actions":[
+    //         {
+    //           "title":"My Account",
+    //           "type":"nested",
+    //           "call_to_actions":[
+    //             {
+    //               "title":"Pay Bill",
+    //               "type":"postback",
+    //               "payload":"PAYBILL_PAYLOAD"
+    //             },
+    //             {
+    //               "title":"History",
+    //               "type":"postback",
+    //               "payload":"HISTORY_PAYLOAD"
+    //             },
+    //             {
+    //               "title":"Contact Info",
+    //               "type":"postback",
+    //               "payload":"CONTACT_INFO_PAYLOAD"
+    //             }
+    //           ]
+    //         },
+    //         {
+    //           "type":"web_url",
+    //           "title":"Latest News",
+    //           "url":"http://rankworldlive.azurewebsites.net/shareToSocial?ranklistid=3111",
+    //           "webview_height_ratio":"full"
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       "locale":"en_US",
+    //       "composer_input_disabled":false
+    //     }
+    //   ],
+    //
+    // };
     request({
-        uri: 'https://graph.facebook.com/v2.6/me/thread_settings?access_token=' + accessToken,
+        uri: 'https://graph.facebook.com/v2.8/me/thread_settings?access_token=' + accessToken,
         headers: {
             "Content-Type": "application/json"
         },
