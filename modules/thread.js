a71aaa2ed6d930f6cfd4863f2411140e001b69a7
narendranath1150 <narendranath.mca@gmail.com>
2017-04-27 14:54:13 +0530

    new commit

--- modules/thread.js
+++ modules/thread.js
@@ -26,46 +26,39 @@ const persistentMenu = (accessToken) => {
     //           ]
     //         };
     var body = {
-  "persistent_menu":[
-    {
-      "locale":"default",
-      "composer_input_disabled":true,
-      "call_to_actions":[
-        {
-          "title":"My Account",
-          "type":"nested",
-          "call_to_actions":[
+        "persistent_menu": [
             {
-              "title":"Pay Bill",
-              "type":"postback",
-              "payload":"PAYBILL_PAYLOAD"
-            },
-            {
-              "title":"History",
-              "type":"postback",
-              "payload":"HISTORY_PAYLOAD"
-            },
-            {
-              "title":"Contact Info",
-              "type":"postback",
-              "payload":"CONTACT_INFO_PAYLOAD"
+                "locale": "default",
+                "composer_input_disabled": true,
+                "call_to_actions": [
+                    {
+                        "title": "My Account",
+                        "type": "nested",
+                        "call_to_actions": [
+                            {
+                                "title": "Pay Bill",
+                                "type": "postback",
+                                "payload": "PAYBILL_PAYLOAD"
+                            }, {
+                                "title": "History",
+                                "type": "postback",
+                                "payload": "HISTORY_PAYLOAD"
+                            }, {
+                                "title": "Contact Info",
+                                "type": "postback",
+                                "payload": "CONTACT_INFO_PAYLOAD"
+                            }
+                        ]
+                    }, {
+                        "type": "web_url",
+                        "title": "Latest News",
+                        "url": "http://rankworldlive.azurewebsites.net/shareToSocial?ranklistid=3111",
+                        "webview_height_ratio": "tall"
+                    }
+                ]
             }
-          ]
-        },
-        {
-          "type":"web_url",
-          "title":"Latest News",
-          "url":"http://rankworldlive.azurewebsites.net/shareToSocial?ranklistid=3111",
-          "webview_height_ratio":"tall"
-        }
-      ]
-    },
-    {
-      "locale":"zh_CN",
-      "composer_input_disabled":false
-    }
-  ]
-};
+        ]
+    };
     request({
         uri: 'https://graph.facebook.com/v2.8/me/thread_settings?access_token=' + accessToken,
         headers: {
