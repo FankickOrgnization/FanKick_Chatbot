0e87a84f6bdec87140f09b69ac49c901a7633c6d
narendranath1150 <narendranath.mca@gmail.com>
2017-05-17 14:29:59 +0530

    new commit

--- modules/sports.js
+++ modules/sports.js
@@ -101,7 +101,7 @@ const sportscelbrityintro = (messagingEvent, sportscelname) => {
     //setTimeout(sportscelbritydetails(messagingEvent, sportscelname), 60000);
     //sportscelbritydetails(messagingEvent, sportscelname);
     //nextlevel (messagingEvent, sportscelname);
-    setInterval(nextlevel (messagingEvent, sportscelname), 10000);
+    setInterval(nextlevel (messagingEvent, sportscelname), 50000);
     user_favorite_sports_celebrity(messagingEvent, sportscelname)
 }
 
@@ -118,7 +118,7 @@ function nextlevel (messagingEvent, sportscelname){
     };
     fbRquest.callFBAPI(messageData, 'https://graph.facebook.com/v2.6/592208327626213/messages');
     console.log('###############@@@@@@@@@@@@@Execute result in 4 sec%%%%%%%%%%%%%%%%%%%%%%$$$$$$$$$$$$$');
-    setInterval(sportscelbritydetails(messagingEvent, sportscelname), 10000);
+    setInterval(sportscelbritydetails(messagingEvent, sportscelname), 50000);
     //sportscelbritydetails(messagingEvent, sportscelname);
     //user_favorite_sports_celebrity(messagingEvent, sportscelname)
 }
